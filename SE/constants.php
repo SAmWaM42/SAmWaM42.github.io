136d4c51c7988fb43a3663bfdb0e89efe973dfbc
Br4vin <Joseph.Bravin@strathmore.edu>
2024-10-28 16:54:21 +0300

    Tracking leave balances

---
+++ SE/constants.php
@@ -0,0 +1,9 @@
+<?php
+    //Creating constants
+    define('DBTYPE', 'PDO');
+    define('HOSTNAME', '127.0.0.1');
+    define('DBPORT', '3306');
+    define('HOSTUSER', 'root');
+    define('HOSTPASS', 'Baguvix67');
+    define('DBNAME', 'easyleave');
+    ?>
---
+++ SE/load.php
@@ -0,0 +1,4 @@
+<?php
+    require_once "insertdata.php";
+    //$Objinsert = new insertdata($pdo);
+    $ObjDb = new dbconnection();
