3ce62716b7be0eb738f21b7acec24d396e5de9d9
Sharon <sharon.kanyi@strathmore.edu>
2024-10-28 16:49:09 +0300

    last module

---
+++ SEproject/DatabaseConnection/Database.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "timeoff_database";
+    private $username = "root";
+    private $password = "232312";
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
