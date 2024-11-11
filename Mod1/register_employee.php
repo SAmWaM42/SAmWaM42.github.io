0eea69a2f756734927a566fc7881752a305315ff
PhilipMOmwanda <166705416+PhilipMOmwanda@users.noreply.github.com>
2024-11-11 11:01:38 +0300

    changed db_instance connection

--- Mod1/register_employee.php
+++ Mod1/register_employee.php
@@ -4,7 +4,7 @@ require_once 'User.php';
 
 // Initialize Database
 $db_instance = new conn();
-$db_instance->connection('localhost', 'root', 'password', 'your_database_name');
+$db_instance->connection('localhost', 'root', '', 'se');
 $db_conn = $db_instance->get_connection();
 
 // Initialize User class
