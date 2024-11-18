7c26080e87d3e4015f81d1a2b060a284cf6f7464
Ian Chomba <ian.gakuru@strathmore.edu>
2024-11-18 18:23:51 +0300

    Chart

--- module3/main2.js
+++ module3/main2.js
@@ -1,26 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sicktotalDays = 15;
 const sickdaysTaken = 8;
-const sickdaysRemaining = sicktotalDays - sickdaysTaken;
+maternity = 8;
     const ctx = document.querySelector('.my_chart2').getContext('2d');
 
     // Create the pie chart
     const myChart = new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: [' Sick Days Taken', 'Sick Days Remaining'],
+            labels: [' Sick Days Taken', 'Sick Days Remaining', 'maternity'],
             datasets: [{
-                data: [sickdaysTaken, sickdaysRemaining],
+                data: [sicktotalDays, sickdaysTaken, maternity],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)', // Red for Days Taken
-                    'rgba(54, 162, 235, 0.6)' // Blue for Days Remaining
+                    'rgba(255, 99, 13, 2, 0.6)', // Red for Days Taken
+                    'rgba(54, 162, 235, 0.6)', // Blue for Days Remaining
+                   ' rgba(255, 0, 0, 0.6)'//Black
+
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)'
-                ],
+                    'rgba(54, 162, 235, 1)',
+                   ' rgba(255, 0, 0, 0.6)'
+
+                ]
+            }],
                 borderWidth: 1
-            }]
+            
         },
         options: {
             responsive: true,
